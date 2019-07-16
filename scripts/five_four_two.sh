b022b23aeb9c3d5eaa24a417a1ee043a0238114d
RADsec-res-mb-1 <ryan@radsec.net>
2019-07-16 14:15:19 -0400

    Updated to meet CIS CentOS 7 v2.2.0

---
+++ scripts/five_four_two.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for user in `awk -F: '($3 < 1000) {print $1}' /etc/passwd` ; do
+    if [ $user !- "root" ]; then
+        usermod -L $user
+        if [ $user  != "sync" ] && [ $user != "shutdown" ] && [ $user != "halt"];
+        then
+            usermod -s /sbin/nologin $user
+        fi
+    fi
+done
